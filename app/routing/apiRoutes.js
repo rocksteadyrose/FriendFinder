99c1fd5bdd912a8dc41216eebf73d1efaa850b22
Leslie Rose <leslierose@Leslies-MBP.attlocal.net>
2018-05-23 13:24:41 -0500

    functionality improvement

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -17,7 +17,22 @@ module.exports = function (app) {
   // ---------------------------------------------------------------------------
 
   app.get("/api/friends", function (req, res) {
-    // res.json(friendsData);
+    res.json(friendsData);
+
+
+  });
+
+  // API POST Requests
+  // Below code handles when a user submits a form and thus submits data to the server.
+  // In each of the below cases, when a user submits form data (a JSON object)
+  // ...the JSON is pushed to the appropriate JavaScript array
+  // (ex. User fills out a reservation request... this data is then sent to the server...
+  // Then the server saves the data to the tableData array)
+  // ---------------------------------------------------------------------------
+
+  app.post("/api/friends", function (req, res) {
+    // req.body is available since we're using the body-parser middleware
+    friendsData.push(req.body);
 
     var othersUserScores = [];
     var totalDifference = [];
@@ -36,7 +51,7 @@ module.exports = function (app) {
       othersUserScores.push(othersScores)
       otherUsers.push(others)
     }
-    console.log(otherUsers)
+    // console.log(otherUsers)
 
     for (i = 0; i < othersUserScores.length; i++) {
       var compareArray = [];
@@ -60,34 +75,13 @@ module.exports = function (app) {
     // console.log(Math.min(difference))
     var min = Math.min.apply(null, totalDifference);
     var selectedPersonIndex = totalDifference.indexOf(min);
-    console.log(selectedPersonIndex)
+    // console.log(selectedPersonIndex)
 
     var match = otherUsers[selectedPersonIndex];
-    console.log(match)
+    var matchName = otherUsers[selectedPersonIndex].name;
+    var matchPic = otherUsers[selectedPersonIndex].photo;
 
-  });
-
-  // API POST Requests
-  // Below code handles when a user submits a form and thus submits data to the server.
-  // In each of the below cases, when a user submits form data (a JSON object)
-  // ...the JSON is pushed to the appropriate JavaScript array
-  // (ex. User fills out a reservation request... this data is then sent to the server...
-  // Then the server saves the data to the tableData array)
-  // ---------------------------------------------------------------------------
-
-  app.post("/api/friends", function (req, res) {
-    // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
-    // It will do this by sending out the value "true" have a table
-    // req.body is available since we're using the body-parser middleware
-
-    // res.json(true);
-
-
-    // else {
-    //   waitListData.push(req.body);
-    //   res.json(false);
-    // }
-
+    res.json({matchName, matchPic});
   });
 
   // ---------------------------------------------------------------------------
